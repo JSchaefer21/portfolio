22cc14b4dbfb1015491363c663f8c391d3647300
Jordi Schaefer <jschaefer21@gmail.com>
2022-07-28 10:53:17 +0200

    Add egg timer project

--- app/src/components/Projects.jsx
+++ app/src/components/Projects.jsx
@@ -13,7 +13,7 @@ function Projects(){
         <a className='Project Project-1' href="https://stravita.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
             <div className='Project__container'onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} >
                 {!hover && <div> 
-                    <p className='Project__description'><text className='bold'>Stravita</text>, is a clone from strava.</p>
+                    <p className='Project__description'><text className='bold'>Stravita</text>, clone from strava.</p>
                     <p className='Project__description'>It's a social media web aplication (mobile first) designed for register and share sport activities.</p>
                     <p className='Project__description'>Users can track their activities with the main focus on the elevation gain, and distance.</p>
                     <p className='Project__description'>The application shows some data in real time like altitude, distance, time, weather and map including the track registered.</p>
