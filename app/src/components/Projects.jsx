b518ee72190f5845831c0c4945f971d9c5ecef4d
Jordi Schaefer <jschaefer21@gmail.com>
2022-09-05 15:57:16 +0200

    add project links buttons

--- app/src/components/Projects.jsx
+++ app/src/components/Projects.jsx
@@ -5,13 +5,17 @@ function Projects(){
 
     const [hover, setHover] = useState(false)
 
-
+    const handleMoreClick = () =>{
+        if(!hover) setHover(true)
+        else setHover(false)
+    }
+  
     return <div className='Projects mw' >
 
         <h1 className='Projects__title'>PROJECTS</h1>
 
-        <a className='Project Project-1' href="https://stravita.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
-            <div className='Project__container'onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} >
+        <div className='Project Project-1' href="https://stravita.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
+            <div className='Project__container' /* onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} */ >
                 {!hover && <div> 
                     <p className='Project__description'><b className='bold'>Stravita</b>, clone from strava.</p>
                     <p className='Project__description'>It's a social media web aplication (mobile first) designed for register and share sport activities.</p>
@@ -29,28 +33,44 @@ function Projects(){
                     <b className='Project__description_test bold2'> test</b>
                     <b className='Project__description_test'>Password: </b>
                     <b className='Project__description_test bold2'> 00004444</b>
-            </div>       
-        </a>
 
-        <a className='Project Project-2' href="https://eggtimer-app.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
+                    <div className='Project__button-container'>
+                        <a className='Project__button' href="https://stravita.surge.sh/" target="_blank" rel="noreferrer">Visit site</a>    
+                        <button className='Project__button-more' onClick={handleMoreClick}>+</button>    
+                        <a className='Project__button' href="https://github.com/JSchaefer21/stravita" target="_blank" rel="noreferrer">See code</a>    
+                    </div>
+            </div>   
+        </div>
+
+        <div className='Project Project-2' href="https://eggtimer-app.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
             <div className='Project__container'>
                 <p className='Project__description'><b className='bold'>Egg Timer</b>, web application that helps you boil eggs at the specific and perfect point for each one.</p>
                 <p className='Project__description'>There is a countdown timer and some pictures to help you know how cooked your eggs are at that specific time.</p>
                 <p className='Project__description'>The timer is set to user preference and an alarm sounds when it reaches the end.</p>
                 <p className='Project__description'></p>
                 <p className='Project__description'><b className='bold'>Actually working, testing and learning!</b></p>
-            </div>       
-        </a>
 
-        <a className='Project Project-3' href="https://weather-time.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
+                <div className='Project__button-container'>
+                    <a className='Project__button' href="https://eggtimer-app.surge.sh/" target="_blank" rel="noreferrer">Visit site</a>    
+                    <a className='Project__button' href="https://github.com/JSchaefer21/EggTimer" target="_blank" rel="noreferrer">See code</a>    
+                </div>
+            </div>       
+        </div>
+
+        <div className='Project Project-3' href="https://weather-time.surge.sh/" style={{display: "table-cell"}} target="_blank" rel="noreferrer">
             <div className='Project__container'>
                 <p className='Project__description'><b className='bold'>Weather App</b>, web application that gives you the weather at that specific time from anywhere in the world.</p>
                 <p className='Project__description'>Weather can be found searching by name, using the location device, or by clicking anywhere on the map.</p>
                 <p className='Project__description'>There is a button to show and hide the map, the coordinates of the point and altitude are also displayed on the map.</p>
                 <p className='Project__description'></p>
                 <p className='Project__description'><b className='bold'>Checkout GitHub</b> repositorie for more information.</p>
+
+                <div className='Project__button-container'>
+                    <a className='Project__button' href="https://weather-time.surge.sh/" target="_blank" rel="noreferrer">Visit site</a>    
+                    <a className='Project__button' href="https://github.com/JSchaefer21/weather" target="_blank" rel="noreferrer">See code</a>    
+                </div>    
             </div>       
-        </a>
+        </div>
    
     </div>
 } 
