97a8f9eb261420824f0bdde412e0b4fe9f9d6eea
Jordi Schaefer <jschaefer21@gmail.com>
2022-09-08 13:43:56 +0200

    update menu bar functions

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import '../styles/Projects.sass'
+import '../styles/About.sass'
 
 function Projects(){
 
@@ -10,10 +10,17 @@ function Projects(){
         else setHover(false)
     }
   
-    return <div className='Projects mw' >
+    return <div className='About mw' >
 
-        <h1 className='Projects__title'>ABOUT ME</h1>
+        <h1 className='About_title'>ABOUT ME</h1>
 
+        <div className='About_container'>
+            <p>Electronic engineer with experience in the automotive industry, 
+                eager to start a new challenge in the development world and improve my knowledge. </p>
+            <p>I acquired the first basics of programming mainly in C++ across the Degree and 
+                during the last months I have trained in some of the most common languages such 
+                as javascript and ReactJS among others.</p>
+        </div>
 
    
     </div>
