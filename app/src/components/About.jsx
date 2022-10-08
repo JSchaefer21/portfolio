d3b2fd0296425a3501a8ab187d73e1d22e8c431f
Jordi Schaefer <jschaefer21@gmail.com>
2022-10-08 10:15:52 +0200

    Update about me styles

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import {AnimatedOnScroll} from "react-animated-css-onscroll"
 import AwesomeSlider from "react-awesome-slider"
 import "react-awesome-slider/dist/styles.css"
-import 'react-awesome-slider/dist/custom-animations/fall-animation.css'
-import 'react-awesome-slider/dist/custom-animations/open-animation.css'
 import '../styles/About.sass'
+import Hobbies from "./Hobbies"
 
 function About(props){
  
+    const [view, setView] = useState(null)
+
     return <div className='About mw' >
         <h1 className='About_title'>ABOUT ME</h1>
 
@@ -15,102 +17,131 @@ function About(props){
             <div className='line'/>
 
             <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
-                <div className='Container Container-left'>
-                    <p className='Container_title'>MTB Rider</p>
-                    <AwesomeSlider className='aws' animation='openAnimation' bullets={false}>
-                        <div data-src={require("./assets/bike_1.jpg")}/>
-                        <div data-src={require("./assets/bike_2.jpg")}/>
-                        <div data-src={require("./assets/bike_3.jpg")}/>
-                        <div data-src={require("./assets/bike_4.jpg")}/>
-                        <div data-src={require("./assets/bike_5.jpg")}/>
-                    </AwesomeSlider>
-                    <p className='Container_description'>Riding mtb (downhill and enduro) since child! cycling is my favourite sport, I even practice trial for a while and participate in some downhill races during a couple of years.</p>
+                <div className='Container-left'>
+                    <p className='Title Title-small'>HI! 👋 I'm Jordi</p>
+                    <p className='Text Text-bold'>From Barcelona, Spain</p>
+                    <p className='Text'>Here you will find more about me</p>
+                    <p className='Text Text-bold'>This is like a CV in a more interactive way</p>
+                    <p className='Text'>My studies, hobbies, passions, work experience, etc.</p>
+                    <p className='Text Text-bold'>Let's start with my studies, I have a...</p>
                 </div>
             </AnimatedOnScroll>
 
             <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200">
-                <div className='Container Container-right'>
-                    <p className='Container_title'>Degree in Electronics Engineering</p>
-                    <img className='Foto' src={require("./assets/upc.png")}/>
+                <div className='Container-right'>
+                    <p className='Title'>Degree in Electronics Engineering</p>
+                    <img className='Logo-school' src={require("./assets/upc.png")}/>
                     <AwesomeSlider className='aws' bullets={false}>
                         <div data-src={require("./assets/electronic_1.jpg")}/>
                         <div data-src={require("./assets/electronic_2.jpg")}/>
                     </AwesomeSlider>
-                    <p className='Container_description'><b className='des_bold'>Final project: </b>Design and program an Audio board for Arduino DUE capable of processing high resolution audio data (24bits, 48Khz) in real time.</p>
+                    <p className='Text'><b className='Text-bold'>Final project: </b>Design and program an Audio board for Arduino DUE capable of processing high resolution audio data (24bits, 48Khz) in real time.</p>
                 </div>
             </AnimatedOnScroll>
 
             <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
-                <div className='Container Container-left'>
-                    <p className='Container_title'>Pasionate about classics</p>
-                    <AwesomeSlider className='aws' animation='fallAnimation' bullets={false}>
-                        <div data-src={require("./assets/mercedes_1.jpg")}/>
-                        <div data-src={require("./assets/mercedes_2.jpg")}/>
-                        <div data-src={require("./assets/mercedes_3.jpg")}/>
-                        <div data-src={require("./assets/mercedes_4.jpg")}/>
-                    </AwesomeSlider>
-                    <p className='Container_description'>I have always liked classic vehicles, I like how they sound and 
-                    the sensations of driving it as if I were in another time.
-                    I'm a proud owner of a 1979 Mercedes that was always in the family, and some other classic bikes.</p>
+                <div className='Container-left'>
+                    <p className='Text Text-bold'>This was my first contact with programming languages in multiple subjects</p>
+                    <p className='Text'>After graduate I decided to go to germany, start working in the automotive industry</p>
+                    <p className='Text Text-bold'>And improve my German skills</p>
                 </div>
             </AnimatedOnScroll>
 
             <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200">           
-                <div className='Container Container-right'>
-                    <p className='Container_title'>Automotive experience as quality testing engineer</p>
-                    <iframe width="100%" height='330' src="https://www.youtube.com/embed/iS7Cr3piWSQ" 
-                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <p className='Container_description'>Almost five years working in the management and performance of functional, durable and fatigue test 
-                    for SEAT and AUDI projects. Inside of multiple departments with cockpit, console, interior, trunk and bumper parts.
-                    Constantly searching for solutions and optimizations of parts to achieve the high requirements of the company.</p>
+                <div className='Container-right'>
+                    <p className='Title'>Technical support at BMW</p>
+                    <p className='Text Text-small'>EDAG Engineering | July 2016 – May 2017 | Munich (Germany)</p>
+                    <p className='Text'>Management, calibration and maintance of sensors, cables and electronic components for development testing cars.</p>
                 </div>
             </AnimatedOnScroll>
 
             <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
-                <div className='Container Container-left'>
-                    <p className='Container_title'>Mountain lover</p>
-                    <AwesomeSlider className='aws' animation='openAnimation' bullets={false}>
-                        <div data-src={require("./assets/mountain_1.jpg")}/>
-                    </AwesomeSlider>
-                    <p className='Container_description'>Always available for a walk, training hard to improve my marks and endurance. 
-                    I love the sensation of peace and adventure that the mountains transmit.
-                    A good plan to do: spend the night in the pyrenees, without tent, looking at the stars.</p>
+                <div className='Container-left Container-language'>
+                    <p className='Title Title-small'>Languages</p>
+                    <p className='Text'>Spanish · native</p>
+                    <p className='Text'>English · intermediate</p>
+                    <p className='Text'>German · intermediate</p>
+                </div>
+            </AnimatedOnScroll>
+            
+            <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
+                <div className='Container-left'>
+                    <p className='Text Text-bold'>Then I come back to Barcelona and I take a good oportunity to continues working in automotive</p>
+                </div>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200">           
+                <div className='Container-right'>
+                    <p className='Title'>SEAT quality testing engineer</p>
+                    <p className='Text Text-small'>Ferchau Engineering | Jun 2017 – November 2021</p>
+                    <p className='Text'>More than four years working in the management and performance of functional, durable and fatigue test 
+                    for SEAT and AUDI projects. Inside of multiple departments with cockpit, console, interior, trunk and bumper parts.</p>
+                    <iframe width="100%" height='330' src="https://www.youtube.com/embed/iS7Cr3piWSQ" 
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <p className='Text'>Constantly searching for solutions and optimizations of parts to achieve the high requirements of the company.</p>                </div>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
+                <div className='Container-left'>
+                    <p className='Text Text-bold'>Here is where I decided to redirect my career to one of my passions i start with some online intensive-course</p>
                 </div>
             </AnimatedOnScroll>
 
             <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200"> 
-                <div className='Container Container-right'>
-                    <p className='Container_title'>Web Developer Bootcamp</p>
-                    <p className='Container_description des_bold'>More than 480h of programming lessons with a main focus on JavaScript and React</p> 
-                    <p className='Container_description'>HTML, CSS, SASS, JavaScript, React, Node, Mongo, GIT</p> 
-                    <p className='Container_description'>Knowledge with backend, API calls and database (everything implemented in my projects) with tests on each logical file, clean and reusable code, and TDD - agile methodologies.</p> 
-                    <img className='Logo2' src={require("./assets/react.png")}/>
-                    <img className='Logo2' src={require("./assets/node.png")}/>
-                    <img className='Logo2' src={require("./assets/mongo.png")}/>
-                    <AwesomeSlider className='aws' bullets={false} organicArrows={false}>
-                        <div data-src={require("./assets/bootcamp.jpg")}/>
-                    </AwesomeSlider>
-                    <p className='Container_description'><b className='des_bold'>Final project: </b>Web aplication and social media designed 
-                    for register and share sport activities with data visualization in real time. You can find it in the projects section!</p>
-                    <button className='Container_description' onClick={()=>props.handleProjectsClick()}>All about the projects</button> 
+                <div className='Container-right'>
+                    <p className='Title'>CS50 Introduction to Computer Science</p>
+                    <p className='Text Text-small'>Harvard University · edX | February 2022</p> 
+                    <p className='Text Text-bold'>10 weeks of lessons and exercises</p> 
+                    <img className='Logo' src={require("./assets/c.png")}/>
+                    <img className='Logo' src={require("./assets/python.png")}/>
+                    <img className='Logo' src={require("./assets/sql.png")}/>
                 </div>
             </AnimatedOnScroll>
 
+            <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
+                <div className='Container-left'>
+                    <p className='Text Text-bold'>And then direct to a Bootcamp</p>
+                </div>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200"> 
+                <div className='Container-right'>
+                    <p className='Title'>Web Developer Bootcamp</p>
+                    <p className='Text Text-small'>Neoland  |  April 2022 – Jun 2022</p> 
+                    <p className='Text Text-bold'>More than 480h of programming lessons with a main focus on JavaScript and React</p> 
+                    <p className='Text'>HTML, CSS, SASS, JavaScript, React, Node, Mongo, GIT</p> 
+                    <p className='Text'>Knowledge of Frontend and Backend, API calls and database (everything implemented in my projects) with tests on each logical file, clean and reusable code, and TDD - agile methodologies.</p> 
+                    <img className='Logo' src={require("./assets/react.png")}/>
+                    <img className='Logo' src={require("./assets/node.png")}/>
+                    <img className='Logo' src={require("./assets/mongo.png")}/>
+                    <img className='Foto' src={require("./assets/bootcamp.jpg")}/>
+                    <p className='Text'><b className='Text-bold'>Final project: </b>Web aplication and social media designed 
+                    for register and share sport activities with data visualization in real time. You can find it in the projects section!</p>
+                </div>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200"> 
+                <div className='Container-right-2'>
+                    <button className='Projects_button' onClick={()=>props.handleProjectsClick()}>Go to Projects</button> 
+                </div>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200"> 
+                <div className='Container-right'>
+                    <p className='Text Text-big Text-bold'>I'm now READY to WORK with you</p> 
+                </div>
+            </AnimatedOnScroll>
+
+            <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200"> 
+                <div className='Container-left Center'>
+                    <p className='Text'>Want to know more about me?</p>
+                    <button className='Hobbies_button' onClick={()=> setView('Hobbies')}>My Hobbies</button>
+                </div>
+            </AnimatedOnScroll>
+
+            {view === 'Hobbies' && <Hobbies/>}
         </div>
     </div>
 } 
 
 export default About
-
-
-
-{/*             <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
-                <div className='Container Container-left'>
-                    <p className='Container_title'>Mechanic as a hobby</p>
-                    <iframe width="100%" height="330" src="https://www.youtube.com/embed/4g8AQYztO6g" 
-                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <p className='Container_description'>I like to do the maintenance of the vehicles by myself, 
-                    restore bikes and solve the issues in the most cheapest and intelligent way. knowing that your "old vehicles" 
-                    are always ready for a ride because of your own work is one of the best feelings.</p>
-                </div>
-            </AnimatedOnScroll> */}
---
+++ app/src/components/Hobbies.jsx
@@ -0,0 +1,69 @@
+import {AnimatedOnScroll} from "react-animated-css-onscroll"
+import AwesomeSlider from "react-awesome-slider"
+import "react-awesome-slider/dist/styles.css"
+import 'react-awesome-slider/dist/custom-animations/fall-animation.css'
+import 'react-awesome-slider/dist/custom-animations/open-animation.css'
+import '../styles/About.sass'
+
+function Hobbies(){
+ 
+    return <div className='mw' >
+
+        <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200">
+            <div className='Container Container-right'>
+                <p className='Title Title-small'>MTB Rider</p>
+                <AwesomeSlider className='aws' animation='openAnimation' bullets={false}>
+                    <div data-src={require("./assets/bike_1.jpg")}/>
+                    <div data-src={require("./assets/bike_2.jpg")}/>
+                    <div data-src={require("./assets/bike_3.jpg")}/>
+                    <div data-src={require("./assets/bike_4.jpg")}/>
+                    <div data-src={require("./assets/bike_5.jpg")}/>
+                </AwesomeSlider>
+                <p className='Text'>Riding mtb (downhill and enduro) since child! cycling is my favourite sport, I even practice trial for a while and participate in some downhill races during a couple of years.</p>
+            </div>
+        </AnimatedOnScroll>
+
+        <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200">
+            <div className='Container Container-right'>
+                <p className='Title Title-small'>Pasionate about classics</p>
+                <AwesomeSlider className='aws' animation='fallAnimation' bullets={false}>
+                    <div data-src={require("./assets/mercedes_1.jpg")}/>
+                    <div data-src={require("./assets/mercedes_2.jpg")}/>
+                    <div data-src={require("./assets/mercedes_3.jpg")}/>
+                    <div data-src={require("./assets/mercedes_4.jpg")}/>
+                </AwesomeSlider>
+                <p className='Text'>I have always liked classic vehicles, I like how they sound and 
+                the sensations of driving it as if I were in another time.
+                I'm a proud owner of a 1979 Mercedes that was always in the family, and some other classic bikes.</p>
+            </div>
+        </AnimatedOnScroll>
+
+        <AnimatedOnScroll className='Center' animationIn="slideInRight" screenOffset='350' animationInDuration="1200">
+            <div className='Container Container-right'>
+                <p className='Title Title-small'>Mountain lover</p>
+                <AwesomeSlider className='aws' animation='openAnimation' bullets={false}>
+                    <div data-src={require("./assets/mountain_1.jpg")}/>
+                </AwesomeSlider>
+                <p className='Text'>Always available for a walk, training hard to improve my marks and endurance. 
+                I love the sensation of peace and adventure that the mountains transmit.
+                A good plan to do: spend the night in the pyrenees, without tent, looking at the stars.</p>
+            </div>
+        </AnimatedOnScroll>
+
+    </div>
+} 
+
+export default Hobbies
+
+
+
+{/*             <AnimatedOnScroll className='Center' animationIn="slideInLeft" screenOffset='350' animationInDuration="1200">
+                <div className='Container Container-left'>
+                    <p className='Container_title'>Mechanic as a hobby</p>
+                    <iframe width="100%" height="330" src="https://www.youtube.com/embed/4g8AQYztO6g" 
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <p className='Container_description'>I like to do the maintenance of the vehicles by myself, 
+                    restore bikes and solve the issues in the most cheapest and intelligent way. knowing that your "old vehicles" 
+                    are always ready for a ride because of your own work is one of the best feelings.</p>
+                </div>
+            </AnimatedOnScroll> */}
