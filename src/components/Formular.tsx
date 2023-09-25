aa780f2438b80f671cb902eab35bd90adf9f3a47
Jordi Schaefer <jschaefer21@gmail.com>
2023-09-25 17:54:09 +0200

    update

--- src/components/Formular.tsx
+++ src/components/Formular.tsx
@@ -1,48 +1,48 @@
-import React, { useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import '../styles/Formular.sass'
 
 
 function Formular() {
 
-    const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    const [message, setMessage] = useState<String | null>(null)
-    const form = useRef()
+  const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const [message, setMessage] = useState<String | null>(null)
+  const form = useRef()
 
-    const sendEmail = (e: any) => {
-        e.preventDefault()
+  const sendEmail = (e: any) => {
+    e.preventDefault()
 
-        if (e.target.user_name.value === '') {
-            setMessage('Please write a name')
-            return
-        } else if (!EMAIL_REGEX.test(e.target.user_email.value)) {
-            setMessage('Email is not valid')
-            return
-        } else if (e.target.message.value === '') {
-            setMessage('Message is empty')
-            return
-        }
-
-        emailjs.sendForm('contact_service', 'contact_form', form.current || "", 'gG8mWVwYo5nbbNy7d')
-            .then((result) => {
-                setMessage('Email sended')
-                e.target.reset()
-            }, (error) => {
-                setMessage('Error: Email was not send')
-            })
+    if (e.target.user_name.value === '') {
+      setMessage('Please write a name')
+      return
+    } else if (!EMAIL_REGEX.test(e.target.user_email.value)) {
+      setMessage('Email is not valid')
+      return
+    } else if (e.target.message.value === '') {
+      setMessage('Message is empty')
+      return
     }
 
-    return <form className='Formular' ref={form.current} onSubmit={sendEmail}>
-        <h1 className='Formular__title'>CONTACT</h1>
+    emailjs.sendForm('contact_service', 'contact_form', form.current || "", 'gG8mWVwYo5nbbNy7d')
+      .then(() => {
+        setMessage('Email sended')
+        e.target.reset()
+      }, () => {
+        setMessage('Error: Email was not send')
+      })
+  }
 
-        <input className='Formular__input' type='text' name='user_name' placeholder=' Name' />
-        <input className='Formular__input' type='text' name='user_email' placeholder=' Email' />
-        <textarea className='Formular__text' name='message' placeholder=' Your message' />
-        <div className='Formular__footer'>
-            <input className='Formular__button' type='submit' value='Send' />
-            {message && <h2 className='Formular__info'>{message}</h2>}
-        </div>
-    </form>
+  return <form className='Formular' ref={form.current} onSubmit={sendEmail}>
+    <h1 className='Formular__title'>CONTACT</h1>
+
+    <input className='Formular__input' type='text' name='user_name' placeholder=' Name' />
+    <input className='Formular__input' type='text' name='user_email' placeholder=' Email' />
+    <textarea className='Formular__text' name='message' placeholder=' Your message' />
+    <div className='Formular__footer'>
+      <input className='Formular__button' type='submit' value='Send' />
+      {message && <h2 className='Formular__info'>{message}</h2>}
+    </div>
+  </form>
 }
 
 export default Formular
